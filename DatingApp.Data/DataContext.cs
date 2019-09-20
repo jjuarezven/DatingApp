3b68f1ddc193eb5fd957422e0ef56fd7ed01ad06
Jose G. Juarez R <josejuarez@outlook.com>
2019-09-20 16:03:51 -0500

    Initial commit

---
+++ DatingApp.Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
