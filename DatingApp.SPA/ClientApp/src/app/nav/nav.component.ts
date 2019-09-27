99e7f8d2b6559220f6570e9fc14419e8699be499
Jose G. Juarez R <josejuarez@outlook.com>
2019-09-27 15:01:43 -0500

    Adding alertify

--- DatingApp.SPA/ClientApp/src/app/nav/nav.component.ts
+++ DatingApp.SPA/ClientApp/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,15 +10,15 @@ import { AuthService } from '../services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(() => {
-        console.log('Logged in succesfully');
-      }, error => console.log(error)
+      this.alertify.success('Logged in succesfully');
+      }, error => this.alertify.error(error)
     );
   }
 
@@ -28,6 +29,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Logged out');
+    this.alertify.message('Logged out');
   }
 }
