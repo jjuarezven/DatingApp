b374b12347e4f4741a1756a5f478ff107807ec15
Jose G. Juarez R <josejuarez@outlook.com>
2019-10-01 14:27:38 -0500

    Creating a new repository

---
+++ DatingApp.Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+    }
+}
