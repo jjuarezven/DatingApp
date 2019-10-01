ac0bc776152d50f71520c3a1f6fbee37c61789e6
Jose G. Juarez R <josejuarez@outlook.com>
2019-10-01 13:57:58 -0500

    Seeding data to db

---
+++ DatingApp.Data/Seed.cs
@@ -0,0 +1,45 @@
+﻿using DatingApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingApp.Data
+{
+    public class Seed
+    {
+        public static void SeedUsers(DataContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userData = File.ReadAllText("../DatingApp.Data/UserSeedData.json");
+                if (!string.IsNullOrEmpty(userData))
+                {
+                    var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                    foreach (var user in users)
+                    {
+                        byte[] passwordHash, passwordSalt;
+                        CreatePasswordHash("password", out passwordHash, out passwordSalt);
+                        user.PasswordHash = passwordHash;
+                        user.PasswordSalt = passwordSalt;
+                        user.UserName = user.UserName.ToLower();
+                        context.Users.Add(user);
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
