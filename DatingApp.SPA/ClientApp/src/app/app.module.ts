8169e60d360e1abf4368841b8bb9c2768e772568
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2019-10-17 20:53:11 -0500

    Adding a CanDeactivate route guard to prevent unsaved changes be lost when navegating to other route

--- DatingApp.SPA/ClientApp/src/app/app.module.ts
+++ DatingApp.SPA/ClientApp/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 
 export class CustomHammerConfig extends HammerGestureConfig {
   overrides = {
@@ -72,7 +73,8 @@ export function tokenGetter() {
     MemberDetailResolver,
     MemberListResolver,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig },
-    MemberEditResolver
+    MemberEditResolver,
+    PreventUnsavedChanges
   ],
   bootstrap: [AppComponent]
 })
