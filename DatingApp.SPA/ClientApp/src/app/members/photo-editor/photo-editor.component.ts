8a41675292595d4eaf521ef1cc40140bd8f72705
JOSE-LAPTOP\Jose <josejuarez78@gmail.com>
2019-11-14 21:48:58 -0500

    Fixing issue when loading files

--- DatingApp.SPA/ClientApp/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.SPA/ClientApp/src/app/members/photo-editor/photo-editor.component.ts
@@ -17,7 +17,10 @@ export class PhotoEditorComponent implements OnInit {
   baseUrl = environment.apiUrl;
   response: string;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
@@ -32,51 +35,13 @@ export class PhotoEditorComponent implements OnInit {
       url: `${this.baseUrl}users/${this.authService.decodedToken.nameid}/photos`,
       authToken: `Bearer ${localStorage.getItem('token')}`,
       isHTML5: true,
-      allowedFileType: ['image'],
-      removeAfterUpload: true,
-      autoUpload: false,
-      maxFileSize: 10 * 1024 * 1024,
-      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-      formatDataFunctionIsAsync: true,
-      formatDataFunction: async item => {
-        return new Promise((resolve, reject) => {
-          resolve({
-            name: item._file.name,
-            length: item._file.size,
-            contentType: item._file.type,
-            date: new Date()
-          });
-        });
-      }
+      allowedMimeType: ['image/jpeg', 'images', 'png', 'jpg'],
+      maxFileSize: 10 * 1024 * 1024, // it will make the maximum file size of 10mbs
+      autoUpload: false, // click a button in order to send this up
+      removeAfterUpload: true // after the photo is being uploaded we want to remove it from the upload queue
     });
-    this.hasBaseDropZoneOver = false;
-    this.response = '';
-    this.uploader.response.subscribe(res => (this.response = res));
-
     this.uploader.onAfterAddingFile = file => {
       file.withCredentials = false;
     };
-
-    this.uploader.onWhenAddingFileFailed = fileNotSupported => {
-      this.alertify.error('The file you are trying to upload is not an image');
-    };
-
-    this.uploader.onSuccessItem = (item, response, status, headers) => {
-      console.log('on success item');
-      if (response) {
-        const res: Photo = JSON.parse(response);
-        const photo = {
-          id: res.id,
-          url: res.url,
-          dateAdded: res.dateAdded,
-          description: res.description,
-          isMain: res.isMain
-        };
-        this.photos.push(photo);
-      }
-      this.alertify.success('photo uploaded');
-    };
   }
-
-
 }
